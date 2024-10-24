3361e42aa026242efef5f4e1d408b0aed4c2173e
Apip Kurniawan <davitaja92@gmail.com>
2024-10-24 08:56:10 +0700

    fix : login

--- src/pages/login/components/LoginForm.tsx
+++ src/pages/login/components/LoginForm.tsx
@@ -1,9 +1,12 @@
 // components/LoginForm.tsx
+import { useRouter } from "next/router";
 import React, { useState } from "react";
 // import axios from "axios";
 
 const LoginForm: React.FC = () => {
-  const [email, setEmail] = useState("");
+  const router = useRouter();
+
+  const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
   const [error, setError] = useState("");
 
@@ -11,6 +14,7 @@ const LoginForm: React.FC = () => {
     e.preventDefault();
     setError("");
 
+    router.push("/");
     // try {
     //   const response = await axios.post("/api/login", { email, password });
     //   console.log(response.data);
@@ -30,15 +34,15 @@ const LoginForm: React.FC = () => {
           htmlFor="email"
           className="block text-sm font-medium text-gray-700"
         >
-          Email
+          Username
         </label>
         <input
-          type="email"
-          id="email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
+          type="username"
+          id="username"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
           required
-          className="mt-1 block w-full border-gray-300 rounded-md shadow-sm focus:border-blue-500 focus:ring focus:ring-blue-500 focus:ring-opacity-50"
+          className="mt-1 block border p-3 w-full border-gray-300 outline-none rounded-md shadow-sm focus:border-blue-500 focus:ring focus:ring-blue-500 focus:ring-opacity-50"
         />
       </div>
       <div>
@@ -54,13 +58,13 @@ const LoginForm: React.FC = () => {
           value={password}
           onChange={(e) => setPassword(e.target.value)}
           required
-          className="mt-1 block w-full border-gray-300 rounded-md shadow-sm focus:border-blue-500 focus:ring focus:ring-blue-500 focus:ring-opacity-50"
+          className="mt-1 block w-full p-3 border border-gray-300 rounded-md shadow-sm focus:border-blue-500 focus:ring focus:ring-blue-500 focus:ring-opacity-50 outline-none"
         />
       </div>
       {error && <p className="text-red-500">{error}</p>}
       <button
         type="submit"
-        className="w-full py-2 px-4 bg-blue-600 text-white font-semibold rounded-md shadow hover:bg-blue-500 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50"
+        className="w-full py-2 px-4 bg-orange-600 text-white font-semibold rounded-md shadow hover:bg-orange-400 focus:outline-none focus:ring-2 focus:ring-orange-300 focus:ring-opacity-50"
       >
         Login
       </button>
