dbf25a995bc1f6ec23bc4f0e782d0d4f03cad4fe
Apip Kurniawan <davitaja92@gmail.com>
2024-10-24 11:30:25 +0700

    set api login

--- src/pages/api/login.ts
+++ src/pages/api/login.ts
@@ -1,12 +1,8 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
-  console.log("req", req);
   if (req.method === "POST") {
     const { username, password } = req.body;
-
-    console.log("username", username);
-    console.log("password", password);
     if (username === "apip_k" && password === "password123") {
       return res.status(200).json({ message: "Login successful" });
     } else {
