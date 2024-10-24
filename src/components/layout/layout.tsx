9a4959979ac52191a95efb170775a7677e63bd08
Apip Kurniawan <davitaja92@gmail.com>
2024-10-24 15:33:57 +0700

    add react idle time

--- src/components/layout/layout.tsx
+++ src/components/layout/layout.tsx
@@ -1,10 +1,24 @@
+import { useIdleTimer } from "react-idle-timer";
 import Footer from "./Footer";
 import Navbar from "./Navbar";
+import { useRouter } from "next/router";
 
 type LayoutProps = {
   children: React.ReactElement;
 };
 export default function Layout({ children }: LayoutProps) {
+  const router = useRouter();
+
+  const onIdle = () => {
+    router.push("/login");
+  };
+
+  useIdleTimer({
+    onIdle,
+    timeout: 60000,
+    throttle: 500,
+  });
+
   return (
     <>
       <Navbar />
