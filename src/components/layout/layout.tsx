ccab40e614a826fec801a78ea559d3b09fd13008
Apip Kurniawan <davitaja92@gmail.com>
2024-10-24 13:37:30 +0700

    add type

--- src/components/layout/layout.tsx
+++ src/components/layout/layout.tsx
@@ -1,7 +1,10 @@
 import Footer from "./footer";
 import Navbar from "./navbar";
 
-export default function Layout({ children }) {
+type LayoutProps = {
+  children: React.ReactElement;
+};
+export default function Layout({ children }: LayoutProps) {
   return (
     <>
       <Navbar />
