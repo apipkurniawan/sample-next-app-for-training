55ac544f8d113779d23730d0379ed0a0b17a3e76
Apip Kurniawan <davitaja92@gmail.com>
2024-10-24 09:46:59 +0700

    refactor

---
+++ src/components/ui/toggle.tsx
@@ -0,0 +1,26 @@
+// components/Toggle.tsx
+import React from "react";
+
+interface ToggleProps {
+  isChecked: boolean;
+  onToggle: (checked: boolean) => void;
+}
+
+const Toggle: React.FC<ToggleProps> = ({ isChecked, onToggle }) => {
+  return (
+    <div
+      onClick={() => onToggle(!isChecked)}
+      className={`flex items-center cursor-pointer ${
+        isChecked ? "bg-blue-600" : "bg-gray-300"
+      } w-12 h-6 rounded-full transition duration-200`}
+    >
+      <div
+        className={`w-6 h-6 bg-white rounded-full shadow-md transform transition duration-200 ${
+          isChecked ? "translate-x-6" : "translate-x-0"
+        }`}
+      />
+    </div>
+  );
+};
+
+export default Toggle;
