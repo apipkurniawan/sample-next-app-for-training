e36c096ba20ac4e71f0a6b9ac43b8d503c6c4262
Apip Kurniawan <davitaja92@gmail.com>
2024-10-24 11:30:07 +0700

    set api login

--- src/pages/api/login.ts
+++ src/pages/api/login.ts
@@ -1,12 +1,13 @@
-// pages/api/login.ts
 import type { NextApiRequest, NextApiResponse } from "next";
 
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
+  console.log("req", req);
   if (req.method === "POST") {
-    const { email, password } = req.body;
+    const { username, password } = req.body;
 
-    // Example authentication logic (replace with your own)
-    if (email === "test@example.com" && password === "password123") {
+    console.log("username", username);
+    console.log("password", password);
+    if (username === "apip_k" && password === "password123") {
       return res.status(200).json({ message: "Login successful" });
     } else {
       return res.status(401).json({ message: "Invalid credentials" });
